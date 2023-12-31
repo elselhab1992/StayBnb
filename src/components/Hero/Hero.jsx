54cf6539cb16488d7897f1cad3850fad57e9cfe3
Mohamed Elselhab <momanchester1992@gmail.com>
2023-12-31 12:26:13 +0000

    create hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import {
+  Section,
+  Heading,
+  Container,
+  SearchIcon,
+  Content,
+  Input,
+  Label,
+  InputContainer,
+  SearchIconContainer,
+} from "./HeroStyles";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <Section>
+      <Heading>find your next stay</Heading>
+      <Container>
+        <InputContainer>
+          <Label>location</Label>
+          <Input type="text" placeholder="Where Are You Going?" />
+        </InputContainer>
+        <Content>
+          <InputContainer>
+            <Label>check in</Label>
+            <Input type="text" placeholder="Add Date" />
+          </InputContainer>
+        </Content>
+        <Content>
+          <InputContainer>
+            <Label>check out</Label>
+            <Input type="text" placeholder="Add Date" />
+          </InputContainer>
+        </Content>
+        <Content>
+          <InputContainer>
+            <Label>guests</Label>
+            <Input type="text" placeholder="Add Guests" />
+          </InputContainer>
+        </Content>
+        <SearchIconContainer>
+          <SearchIcon />
+        </SearchIconContainer>
+      </Container>
+    </Section>
+  );
 };
 
 export default Hero;
---
+++ src/components/Hero/HeroStyles.jsx
@@ -0,0 +1,160 @@
+import styled from "styled-components";
+import { IoSearch } from "react-icons/io5";
+import heroImg from "../../assets/banner.png";
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url(${heroImg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media (min-width: 300px) {
+    gap: 10px;
+  }
+
+  @media (min-width: 768px) {
+    gap: 1rem;
+    padding: 0 10px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 0;
+    gap: 1rem;
+  }
+`;
+
+const Heading = styled.h1`
+  font-weight: ${(props) => props.theme.fontWeight.medium};
+  font-size: ${(props) => props.theme.fontSize.xl};
+  color: ${(props) => props.theme.colors.secondary};
+  margin-top: 15rem;
+
+  @media (min-width: 300px) {
+    font-weight: ${(props) => props.theme.fontWeight.extraBold};
+    font-size: ${(props) => props.theme.fontSize.medium};
+  }
+
+  @media (min-width: 768px) {
+    font-weight: ${(props) => props.theme.fontWeight.medium};
+    font-size: ${(props) => props.theme.fontSize.xl};
+  }
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr 2fr 0.5fr;
+  padding: 1rem;
+  border-radius: 5rem;
+  background-color: ${(props) => props.theme.colors.secondary};
+  position: relative;
+
+  @media (min-width: 300px) {
+    grid-template-columns: 1fr;
+    gap: 5px;
+    border-radius: 5px;
+    width: 70%;
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(4, 1fr);
+    width: 100%;
+    border-radius: 5rem;
+  }
+
+  @media (min-width: 960px) {
+    grid-template-columns: 2fr 1fr 1fr 2fr;
+  }
+
+  @media (min-width: 1100px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 0.5fr;
+    border-radius: 5rem;
+    width: 90%;
+  }
+
+  @media (min-width: 1600px) {
+    grid-template-columns: 3fr 1fr 1fr 3fr 1fr;
+    padding: 0.5rem;
+    width: 55%;
+  }
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 5px;
+`;
+
+const Label = styled.label`
+  font-weight: ${(props) => props.theme.fontWeight.medium};
+  font-size: ${(props) => props.theme.fontSize.small};
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  background-color: ${(props) => props.theme.colors.secondary};
+
+  @media (min-width: 768px) and (max-width: 900px) {
+    width: 100%;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  @media (min-width: 300px) {
+    justify-content: flex-start;
+    border-top: 1px solid #ccc;
+    padding: 8px;
+  }
+
+  @media (min-width: 768px) {
+    border-top: none;
+    border-left: 1px solid #ccc;
+  }
+`;
+
+const SearchIconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    position: absolute;
+    bottom: -1.5rem;
+    left: 45%;
+  }
+`;
+
+const SearchIcon = styled(IoSearch)`
+  font-size: 50px;
+  color: ${(props) => props.theme.colors.secondary};
+  background-color: ${(props) => props.theme.colors.buttons};
+  border-radius: 50%;
+  padding: 1rem;
+`;
+
+export {
+  Section,
+  Heading,
+  Container,
+  Input,
+  SearchIcon,
+  Content,
+  Label,
+  InputContainer,
+  SearchIconContainer,
+};
